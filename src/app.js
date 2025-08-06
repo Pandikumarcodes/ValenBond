44bb112ae5ecd5d1f270df05acc632b0e324acf2
PandiKumar-Dev <developerpk7@gmail.com>
2025-08-06 19:22:15 +0530

    create  a get and post api

--- src/app.js
+++ src/app.js
@@ -4,18 +4,43 @@ const app = express();
 const PORT = 7498;
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const newUser = new User({
-    firstName: "Pandi",
-    lastName: "Selvam",
-    emailId: "pandi@example.com",
-    password: "secure123",
-    age: 23,
-    gender: "Male",
-  });
+  const newUser = new User(req.body);
   try {
     await newUser.save();
     res.send("data posted");
+  } catch (err) {
+    res.status(400).send("error", err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmails = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmails });
+    console.log(users);
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("error", err.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  const userEmails = req.body.emailId;
+  try {
+    const users = await User.findOne({ emailId: userEmails });
+    console.log(users);
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
+    }
   } catch (err) {
     res.status(400).send("error", err.message);
   }
