01446797dbeb2914588e8acf045360c8c4ec3e29
PandiKumar-Dev <developerpk7@gmail.com>
2025-08-07 14:04:10 +0530

    Data sanitization and schema validation

--- src/app.js
+++ src/app.js
@@ -6,16 +6,22 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+//Post api
 app.post("/signup", async (req, res) => {
   const newUser = new User(req.body);
+  console.log(newUser);
   try {
     await newUser.save();
     res.send("data posted");
   } catch (err) {
-    res.status(400).send("error", err.message);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: "Email already exists" });
+    }
+    res.status(400).json({ error: err.message });
   }
 });
 
+// get api by emal
 app.get("/user", async (req, res) => {
   const userEmails = req.body.emailId;
   try {
@@ -31,16 +37,37 @@ app.get("/user", async (req, res) => {
   }
 });
 
-app.get("/users", async (req, res) => {
-  const userEmails = req.body.emailId;
+//Update Id
+app.patch("/users/:userId", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  console.log(data);
   try {
-    const users = await User.findOne({ emailId: userEmails });
-    console.log(users);
-    if (users.length === 0) {
-      res.status(404).send("user not found");
-    } else {
-      res.send(users);
+    const Allowed_Updates = ["photoUrl", "gender", "age", "skills", "emailId"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      Allowed_Updates.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not Allowed");
     }
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log("user");
+    res.send("user updated successfully");
+  } catch (err) {
+    res.status(400).send("error", err.message);
+  }
+});
+
+// Delete Api
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const users = await User.findByIdAndDelete(userId);
+    console.log("deleted");
+    res.send(users);
   } catch (err) {
     res.status(400).send("error", err.message);
   }
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,71 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password " + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender Data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.kindpng.com/picc/m/252-2524695_dummy-profile-image-jpg-hd-png-download.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid photo url address " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is default user",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
