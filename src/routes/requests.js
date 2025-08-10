45e9c6ac4c49786fcc28d0496c1aeb82c1902ae2
PandiKumar-Dev <developerpk7@gmail.com>
2025-08-10 15:39:02 +0530

    created the connectionrequestApi

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,12 +1,67 @@
 const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
-requestRouter.post("/sendConnection", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-    res.send(user.firstName + " is Logging");
-  } catch (err) {
-    res.status(400).send("error : " + err.message);
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "Invalid status type: " + status,
+        });
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          {
+            fromUserId: toUserId,
+            toUserId: fromUserId,
+          },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        return res.status(400).json({
+          message: "connection Request Already Exists",
+        });
+      }
+
+      // if (fromUserId == toUserId) {
+      //   return res.status(400).json({
+      //     message: "This request made by same UserId",
+      //   });
+      // }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("error : " + err.message);
+    }
   }
-});
+);
 module.exports = { requestRouter };
