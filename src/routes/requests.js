936e255bee0f56f40ab9ce7a77b1f226c8146e7c
PandiKumar-Dev <developerpk7@gmail.com>
2025-08-11 17:28:20 +0530

    creates users  connections

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -3,6 +3,7 @@ const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const User = require("../models/user");
+
 requestRouter.post(
   "/request/send/:status/:toUserId",
   userAuth,
@@ -64,4 +65,47 @@ requestRouter.post(
     }
   }
 );
+
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: " status not allowed: " + status,
+        });
+      }
+      const connectionRequest = await ConnectionRequest.findOne({
+        fromUserId: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res.status(400).json({
+          message: " connection request not found ",
+        });
+      }
+
+      connectionRequest.status = status;
+
+      console.log(connectionRequest);
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: "connection request " + status,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("error : " + err.message);
+    }
+  }
+);
+
 module.exports = { requestRouter };
